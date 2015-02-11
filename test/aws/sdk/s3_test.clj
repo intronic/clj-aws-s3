2425d053acbb73f02b6c591140f84e0532d675f0
Michael Pheasant <mpheasant@gmail.com>
2015-02-11 11:34:34 +1000

    added time test

--- test/aws/sdk/s3_test.clj
+++ test/aws/sdk/s3_test.clj
@@ -1,8 +1,14 @@
 (ns aws.sdk.s3-test
   (require [clojure.test :refer :all]
-           [aws.sdk.s3 :as s3 :refer :all]))
+           [aws.sdk.s3 :as s3 :refer :all]
+           [clj-time.core :as t]
+           [clj-time.coerce :as coerce]))
 
 (deftest a-test
   (testing "maybe-int"
     (is (nil? (#'s3/maybe-int nil)))
     (is (= 1 (#'s3/maybe-int 1)))))
+
+(deftest time-test
+  (testing "from-now"
+    (is (t/from-now nil))))
